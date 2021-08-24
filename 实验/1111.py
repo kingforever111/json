9485b66bf0f8d4b291dea4d2c3349cdcd3f105e2
Shu zhan <shzh19970901@gmail.com>
2021-08-24 15:20:26 +0800

    测试一下

---
+++ 实验/1111.py
@@ -0,0 +1,13 @@
+import os
+counts = 0
+log_dir = 'C:/Users/Administrator/Desktop/暂存'
+a = os.listdir(log_dir)
+for i in a:
+    file_path = f'{log_dir}/{i}'
+    with open(file_path, 'r') as f:
+        for line in f:
+            if "ERROR" in line:
+                counts += 1
+print(counts)
+
+
